ee447a7e04db9dca0a5a9508a2c6947c8f239339
zhangpanpan1 <zjrzpp0414@163.com>
2019-05-09 13:48:44 +0800

    api use2

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ App({
   onLaunch(options) {
     // 小程序启动之后 触发
     console.log(options)
+    // 获得场景值
+    console.log(options.scene)
   },
   globalData: {
     g_isPlayingMusic: false,
--- pages/demo/api/api.js
+++ pages/demo/api/api.js
@@ -18,14 +18,88 @@ Page({
   },
   // 用户设置界面
   onBindUserSet() {
-
+    // 调起客户端小程序设置界面
+    wx.openSetting({
+      success: (res) => {
+        console.log(res);
+      }
+    })
+    // 获得用户的当前设置
+    // wx.getSetting({
+    //   success: function(res) {
+    //     // 用户授权的结果
+    //     console.log(res.authSetting)
+    //     console.log(res.authSetting['scope.userInfo'])
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 获取用户信息(明文信息)
+    //       wx.getUserInfo({
+    //         // withCredential为true，必须要求用户登录
+    //         withCredentials: true,
+    //         success(res) {
+    //           console.log(res)
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
   },
   // 检查登录状态
   onBindLogin() {
+    // 不需要授权，静默登录
+    // 登录状态有时效性
+    // 多久失效由服务器决定
+    // wx.login({
+    //   success: function(res) {
+    //     // console.log(res);
+    //     // 用户登录的凭证（有效期五分钟）
+    //     console.log(res.code)
+    //     if(res.code) {
 
+    //     } else {
+    //       console.log('登录失败！'+ res.errMsg)
+    //     }
+    //   }
+    // })
+    // 可查看是否失效 
+    wx.checkSession({
+      success: function() {
+        console.log('登录未过期')
+      },
+      fail: function() {
+        // 重新登录
+        wx.login({
+          success: function (res) {
+            // console.log(res);
+            // 用户登录的凭证（有效期五分钟）
+            console.log(res.code)
+            if (res.code) {
+
+            } else {
+              console.log('登录失败！' + res.errMsg)
+            }
+          }
+        })
+      
+      }
+    })
   },
   // 获得用户信息
   onBindUserInfo() {
+    wx.login({
+      success() {
+        wx.getUserInfo({
+          withCredentials: true,
+          success(res) {
+            console.log(res)
+          }
+        })
+      }
+    })
+  },
+  onGotUserInfo(e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.userInfo)
+    console.log(e.detail.rawData)
 
   },
   // 开始录音
@@ -34,10 +108,26 @@ Page({
   },
   // 设置分享 
   onBindSetShare() {
-
+    wx.showShareMenu({
+      withShareTicket: true,
+      success: (res) => {
+        console.log(res);
+      }
+    })
   },
   // 取消分享
   onBindCancelShare() {
-
+    // 隐藏转发按钮
+    wx.hideShareMenu()
+  },
+  onShareAppMessage: function(res) {
+    if(res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '自定义转发标题',
+      path: '/pages/demo/form'
+    }
   }
 })
